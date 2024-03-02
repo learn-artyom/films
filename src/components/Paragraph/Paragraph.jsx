ab063f616d586951793d73839a09729e7fb67453
Artyom <artyom@inbox.ru>
2024-03-03 00:09:06 +0300

    [1-start] размер параграфа

--- src/components/Paragraph/Paragraph.jsx
+++ src/components/Paragraph/Paragraph.jsx
@@ -1,7 +1,9 @@
 import './Paragraph.css';
 
-function Paragraph({ children }) {
-  return <p className="paragraph">{children}</p>;
+function Paragraph({ children, size = 'sm' }) {
+  const className = 'paragraph paragraph--' + size;
+  
+  return <p className={className}>{children}</p>;
 }
 
 export default Paragraph;
